c8dba621d64275f652b5b59b7c148f915bf5cc01
Mads Jon Nielsen <mjn@cego.dk>
2023-02-04 20:28:11 +0100

    Add proper http timeout numbers

--- src/debug/requester.js
+++ src/debug/requester.js
@@ -11,12 +11,14 @@ export async function handler(argv) {
     process.on("SIGTERM", () => shuttingDown = true);
 
     while (!shuttingDown) {
-        const [err, res] = await to(got.get(argv["url"], {timeout: {lookup: 100, connect: 100}}));
+        const [err, res] = await to(got.get(argv["url"], {timeout: {lookup: 100, connect: 100, socket: 100}}));
         if (err) {
-            return console.error(`msg="request failed" ${err.message} service.type=requester`);
+            console.error(`msg="request failed" ${err.message} service.type=requester`);
+            continue;
         }
         if (res.statusCode !== 200) {
-            return console.error(`msg="request failed" status_code=${res.statusCode} service.type=requester`);
+            console.error(`msg="request failed" status_code=${res.statusCode} service.type=requester`);
+            continue;
         }
         console.log(`msg="request succeeded" status_code=${res.statusCode} service.type=requester`);
         await wait.sleep(argv["sleep"]);
